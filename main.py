2810cbada79cb3c80d9f9945aac4c8c63a5e982e
Samyak Kakatur <107428179+therealsamyak@users.noreply.github.com>
2023-04-29 16:07:14 -0700

    Update main.py
    Improved UI

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pygame
+import time
 import random
 import numpy as np
 
-random.seed(0)
-
-ROW_COUNT = 6
-COLUMN_COUNT = 7
+random.seed(time.time())
 
 class board:
     #Default Constructor (doesn't return )
@@ -15,6 +13,8 @@ class board:
     #     self.game_over = False #game_over for this board
     
     def __init__(self, num_rows, num_columns):
+        self.num_rows = num_rows
+        self.num_columns = num_columns
         self.board = []
         column = []
         i = 0
@@ -32,14 +32,14 @@ class board:
 
     def valid(self, board, column):
         column -= 1
-        for r in range(ROW_COUNT):
+        for r in range(self.num_rows):
             if self.board[r][column] == "0":
                 return True
         return False
     
     def place_coin(self, column, piece):
         
-        if (column > COLUMN_COUNT or column <= 0):
+        if (column > self.num_columns or column <= 0):
             print("Invalid Column")
             return
         column -= 1
@@ -51,18 +51,18 @@ class board:
             self.board[self.get_first_empty_spot_in_column(column)][column] = piece
 
     def remove_Coin(self, column):
-        if (column > COLUMN_COUNT or column <= 0):
+        if (column > self.num_columns or column <= 0):
             print("Invalid Column")
             return
         column -= 1
         self.board[0][column] = 0
-        for r in range(1, ROW_COUNT - 1):
+        for r in range(1, self.num_rows - 1):
             self.board[r - 1][column] = self.board[r][column]
         
-        self.board[ROW_COUNT - 1][column] = "0"
+        self.board[self.num_rows - 1][column] = "0"
 
     def get_first_empty_spot_in_column(self, column):
-        for i in range(ROW_COUNT):
+        for i in range(self.num_rows):
             if self.board[i][column] == "0":
                 return i
         
@@ -74,9 +74,9 @@ class board:
     
     #getter function for print board
     def print_board(self):
-        for i in range(ROW_COUNT - 1, -1, -1):
-            print("|", end="")
-            for c in range(COLUMN_COUNT):
+        for i in range(self.num_rows - 1, -1, -1):
+            print("| ", end="")
+            for c in range(self.num_columns):
                 print(self.board[i][c], end = " ")
             print("|")
 
@@ -84,9 +84,9 @@ class board:
     
     def check_tie(self):
         checker_Full = True
-        for i in range(ROW_COUNT):
-            for j in range(COLUMN_COUNT):
-                if self.board[i][j] == '0':
+        for i in range(self.num_rows):
+            for j in range(self.num_columns):
+                if self.board[i][j] == "0":
                     checker_Full = False
                     break
         return checker_Full
@@ -97,46 +97,49 @@ class board:
 
         r = 0
         #vertically bottom-up except while loop
-        while (r + 3 < ROW_COUNT):
-            for columns in range(COLUMN_COUNT):
+        while (r + 3 < self.num_rows):
+            for columns in range(self.num_columns):
                 # print(r, c)
                 if self.board[r][columns] == coin and self.board[r+1][columns] == coin and self.board[r+2][columns] == coin and self.board[r+3][columns] == coin:
                     return True
             r += 1
         #horizontally left-right except while loop
         c = 0
-        while(c + 3 < COLUMN_COUNT):
-            for rows in range(ROW_COUNT):
+        while(c + 3 < self.num_columns):
+            for rows in range(self.num_rows):
                 if self.board[rows][c] == coin and self.board[rows][c+1] == coin and self.board[rows][c+2] == coin and self.board[rows][c+3] == coin:
                     return True
             c += 1
         
         #check for a positive slope diagnal
-        for c in range(COLUMN_COUNT-3):
-            for r in range(ROW_COUNT-3):
+        for c in range(self.num_columns-3):
+            for r in range(self.num_rows-3):
                 if self.board[r][c] == coin and self.board[r+1][c+1] == coin and self.board[r+2][c+2] == coin and self.board[r+3][c+3] == coin:
                     return True
         
         #check for a negative slope
-        for c in range(COLUMN_COUNT-3):
-            for r in range(3, ROW_COUNT):
+        for c in range(self.num_columns-3):
+            for r in range(3, self.num_rows):
                 if self.board[r][c] == coin and self.board[r-1][c+1] == coin and self.board[r-2][c+2] == coin and self.board[r-3][c+3] == coin:
                     return True
         
         return False
 
 
-def PvP():
-    Board = board(6, 7)
-    turn = 0
+def PvP(num_rows, num_columns):
+    Board = board(num_rows, num_columns)
+    turn = 2
     print("Player 1 and Player 2 will enter a character symbol for their coin")
     player_one_symbol = input("Player 1: Please enter your symbol\n")
     player_two_symbol = input("Player 2: Please enter your symbol\n")
     
-    while player_one_symbol == player_two_symbol and player_one_symbol != '0' and player_two_symbol != '0':
+    while player_one_symbol == player_two_symbol and player_one_symbol != "0" and player_two_symbol != "0":
         print("Same symbols or one or more players' symbols are 0")
         player_one_symbol = input("Player 1: Please enter your symbol\n")
         player_two_symbol = input("Player 2: Please enter your symbol\n")
+
+        player_one_symbol = " " + player_one_symbol + " "
+        player_two_symbol = " " + player_two_symbol + " "
     
     running = True
     while (running):
@@ -145,15 +148,14 @@ def PvP():
         print("1. Place coin")
         print("2. Pop coin")
         print("3. Quit")
-        userinput = input("What do you want to do with your coin\n")
-
+        userinput = input("Player " + str((turn % 2) + 1) + ": What do you want to do with your coin\n")
 
         if userinput == "3":
             running = False
             break
 
         elif userinput == "1":
-            col = input("Player 1: Select where to drop (1 - " + str(COLUMN_COUNT) + "): ")
+            col = input("Player " + str((turn % 2) + 1) + ": Select where to place (1 - " + str(Board.num_columns) + "): ")
             
             if (not col.isnumeric()):
                 print("Error, invalid input")
@@ -166,14 +168,12 @@ def PvP():
                 turn += 1
         
         elif userinput == "2":
-            col = input("Player 2: Select where to remove (1 - " + str(COLUMN_COUNT) + "): ")
+            col = input("Player " + str((turn % 2) + 1) + ": Select where to remove (1 - " + str(Board.num_columns) + "): ")
 
             if (not col.isnumeric()):
                 print("Error, invalid input")
-                running = False
             else:
                 Board.remove_Coin(int(col))
-                running = False
     
         print()
         Board.print_board()
@@ -188,19 +188,17 @@ def PvP():
         elif Board.check_tie():
             print("We have a tie")
             running = False
+        
 
-def PvC():
-    Board = board(6, 7)
-    turn = 0
+def PvEasy(num_rows, num_columns):
+    Board = board(num_rows, num_columns)
     
-    print("Player, Enter a Symbol")
-    userinput = input()
 
     print("Player will enter a character symbol for their coin\n")
     player_symbol = input("Player: Please enter your symbol\n")
     AI_symbol = "A"
     
-    while player_symbol == AI_symbol and player_symbol != '0':
+    while player_symbol == AI_symbol or player_symbol == "0":
         print("Same symbols or player's symbol is 0\n")
         player_symbol = input("Player: Please enter your symbol\n")
 
@@ -211,7 +209,7 @@ def PvC():
         print("1. Place coin")
         print("2. Pop coin")
         print("3. Quit")
-        userinput = input("What do you want to do with your coin\n")
+        userinput = input("Player: What do you want to do with your coin\n")
 
         if userinput == "3":
             running = False
@@ -219,23 +217,32 @@ def PvC():
 
 
         elif userinput == "1":
-            col = input("Player: Select where to drop coin (1 - " + str(COLUMN_COUNT) + "): ")
-            
+            col = input("Player : Select where to place (1 - " + str(Board.num_columns) + "): ")
             if (not col.isnumeric()):
                 print("Error, invalid input")
             
-        
+            else:
+                Board.place_coin(int(col), player_symbol)
+            
         elif userinput == "2":
-            col = input("Player: Select where to remove coin (1 - " + str(COLUMN_COUNT) + "): ")
+            col = input("Player: Select where to remove coin (1 - " + str(Board.num_columns) + "): ")
 
             if (not col.isnumeric()):
                 print("Error, invalid input")
             else:
                 Board.remove_Coin(int(col))
                 running = False
-    
+
+        AI_column = random.randint(1, Board.num_columns)
+
+        while (Board.get_first_empty_spot_in_column(AI_column - 1) == -1):
+            AI_column = random.randint(1, Board.num_columns)
         
+        Board.place_coin(AI_column, AI_symbol)
+        print("AI Placed Coin in Column: " + str(AI_column) + ".")
+        print()
         Board.print_board()
+        print()
         #Change 2 and 6 to find wins
         if Board.check_win(player_symbol):
             print("The player wins!!")
@@ -247,36 +254,35 @@ def PvC():
             print("We have a tie")
             running = False
             
-    
-def print_menu():
-    print("Welcome to Connect 4++!")
-    
-    running = True
-
-    while running:
-    
-        print("User Menu:")
-        print("1. Player vs AI")
-        print("2. Player vs Player")
-        userInput = input("Please select an option: ") #whatever user inputs will be stored in userInput
-
-        if userInput == "1" or userInput == "2":
-            running = False
-            break
-        
-        print()
-
-    return userInput
 
 
 def main():
+    num_columns = 7
+    num_rows = 6
 
-    userInput = print_menu()
+    print("Welcome to Connect 4++!")
+    
+    running = True
+    while running:
+    
+        print("User Menu:")
+        print("Current Board Size: " str(num_rows) + " x " + str(num_columns))
+        print("1. Player vs AI")
+        print("2. Player vs Player")
+        print("3. Change Board Size")
+        userInput = input("Please select an option: ") #whatever user inputs will be stored in userInput
+
+        if userInput == "1":
+            PvEasy(num_rows, num_columns)
+        
+        elif userInput == "2"
+
+        print()
 
     if userInput == "1":
-        PvC()
+        PvEasy(4, 4)
     elif userInput == "2":
-        PvP()
+        PvP(4, 4)
 
 
     
