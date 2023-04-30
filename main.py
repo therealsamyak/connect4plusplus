1fc7c2ab3a8a6dcd56d03eb8c2bc09e3261ac529
depthcharge78 <132155715+depthcharge78@users.noreply.github.com>
2023-04-29 21:56:48 -0700

    Update main.py
    Finished Ai moderate and added aesthetics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import random
 import copy
+# import pygame
+# pygame.init()
 
 random.seed(time.time())  #seeding the random function to the current time
 
@@ -81,7 +83,7 @@ class board:
 
     def get_first_empty_spot_in_column(self, column):
         '''
-        Returns 
+        Returns the first empty position going up the column
         '''
         for i in range(self.num_rows):
             if self.board[i][column] == "0":
@@ -102,10 +104,10 @@ class board:
         Prints out the current board 
         '''
         for i in range(self.num_rows - 1, -1, -1):
-            print("| ", end="")
+            print("║ ", end="")
             for c in range(self.num_columns):
                 print(self.board[i][c], end = " ")
-            print("|")
+            print("║")
 
 
     
@@ -124,7 +126,9 @@ class board:
 
 
     def check_win(self,coin):
-
+        '''
+        Checks conditions necessary for win: horizontal, vertical, positive slope, and negative slope
+        '''
         r = 0
         #vertically bottom-up except while loop
         while (r + 3 < self.num_rows):
@@ -154,49 +158,71 @@ class board:
                     return True
         
         return False
-    #Helper Functions
+    #Helper Functions only for AI moderate difficulty
     def drop_piece(self,row,col,coin):
+        '''
+        Depending upon if it's player or ai inputs their symbol on that matrix location
+        '''
         self.board[row][col] = coin
 
-    def get_board(self):
-        return self.board
-
     def get_next_open_row(self,col):
+        '''
+        Returns the next empty row index up in the column inputted
+        '''
         for r in range(self.num_rows-1):
             if self.board[r][col] == "0":
                 return r
+            
+        return -1
+        
+        
 
     def is_valid_location(self,col):
+        '''
+        Returns whether the topmost position of the column is empty or not
+        '''
         return self.board[self.num_rows-1][col] == "0"
 
     def get_valid_locations(self):
+        '''
+        Return array of possible columns free that AI can choose from
+        '''
         valid_locations = []
         for col in range(self.num_columns):
-            if self.valid(self,col):
+            if self.valid(col):
                 valid_locations.append(col)
         return valid_locations
     def update_coins(self, aiCoin,playerCoin):
+        '''
+        Updates the characters/ symbols used for the player and ai's turn
+        '''
         AI_COIN = aiCoin
         PLAYER_COIN = playerCoin
     #Helper Function To find Score of player
     def evaluate_window(self, window, player_coin, ai_coin, coinType):
+        '''
+        Returns the score for 4 in a row, 3 in a row, 2 in a row, and finally opponent's score if they had 3 in a row. The score is used by the AI to determine the best possible column to drop its coin in
+        '''
         EMPTY = 0
         score = 0
         opp_coin = player_coin
         if(coinType == player_coin):
             opp_coin = ai_coin
         if window.count(coinType) == 4:
-            score += 100
+            score += 10000
         elif window.count(coinType) == 3 and window.count(EMPTY) == 1:
-            score += 5
+            score += 1000
         elif window.count(coinType) == 2 and window.count(EMPTY) == 2:
-            score += 2
+            score += 250
         if window.count(opp_coin) == 3 and window.count(EMPTY) == 1:
-            score -= 4
+            score -= 40
 
         return score
 
     def score_position(self,coinType):
+        '''
+        Calculates score similar to previous functions for moves done in the center, horizontal, vertical, and diagonals. Adding the score will determine the best possible move used by AI to act "intelligently"
+        '''
         WINDOW_LENGTH = 4
         SCORE = 0
         EMPTY = "0"
@@ -221,28 +247,48 @@ class board:
             for c in range(self.num_columns - 3):
                 window = row_array[c : c + WINDOW_LENGTH]
                 SCORE += self.evaluate_window(window, self.PLAYER_COIN, self.AI_COIN, coinType)
+        
+        #Score positive sloped diagonal
+        for r in range(self.num_rows - 4):
+            for c in range(self.num_columns - 4):
+                window = [self.board[r + i][c + i] for i in range(WINDOW_LENGTH)]
+                SCORE += self.evaluate_window(window, self.PLAYER_COIN, self.AI_COIN, coinType)
+        #Score negatively sloped diagonal
+        for r in range(self.num_rows - 4):
+            for c in range(self.num_columns - 4):
+                window = [self.board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+                SCORE = self.evaluate_window(window, self.PLAYER_COIN, self.AI_COIN, coinType)
         return SCORE
 
     def pick_best_move(self,coinType):
+        '''
+        Out of valid locations AI can put into and depending upon the best possible score choose that position for AI to put its coin there
+        '''
         valid_locations = self.get_valid_locations()
         best_col = random.choice(valid_locations)
         best_score = 0
         for col in valid_locations:
             row = self.get_next_open_row(col)
-            temp_board = self.copy()
-            temp_board.drop_piece(row,col,coinType)
-            score = temp_board.score_position(coinType)
-            if score > best_score:
-                best_score = score
-                best_col = col
+            if (row != -1):
+                
+                temp_board = self.copy()
+                temp_board.drop_piece(row,col,coinType)
+                score = temp_board.score_position(coinType)
+                if score > best_score:
+                    best_score = score
+                    best_col = col
 
         return best_col + 1
 ##
 ##
-##                  NOT IN CLASS AFTER THIS LINE!!!!!!!!!
+##  NOT IN CLASS AFTER THIS LINE!!!!!!!!!
 ##
 ##
+
 def load_words():
+    '''
+    returns list of words that can be used by the AI to make game entertaining from a list of dialogues in Lines.txt generated from ChatGPT
+    '''
     inFile = open("Lines.txt")
     wordlist = []
     for line in inFile:
@@ -253,30 +299,36 @@ def load_words():
 
 
 def PvP(num_rows, num_columns):
+    '''
+    Sets up player vs player game mode
+    '''
     Board = board(num_rows, num_columns)
     turn = 2
     print("Player 1 and Player 2 will enter a character symbol for their coin")
-    player_one_symbol = input("Player 1: Please enter your symbol\n")
-    player_two_symbol = input("Player 2: Please enter your symbol\n")
+    player_one_symbol = input("Player 1, please enter your symbol: ")
+    player_two_symbol = input("Player 2, please enter your symbol: ")
     
-    while player_one_symbol == player_two_symbol and player_one_symbol != "0" and player_two_symbol != "0":
-        print("Same symbols or one or more players' symbols are 0")
-        player_one_symbol = input("Player 1: Please enter your symbol\n")
-        player_two_symbol = input("Player 2: Please enter your symbol\n")
+    while player_one_symbol == player_two_symbol or player_one_symbol == "0" or player_two_symbol == "0":
+        print("Both players have the same symbol, or one of the players has an invalid symbol.")
+        player_one_symbol = input("Player 1, please enter your symbol: ")
+        player_two_symbol = input("Player 2, please enter your symbol: ")
 
-        player_one_symbol = " " + player_one_symbol + " "
-        player_two_symbol = " " + player_two_symbol + " "
     
     running = True
     while (running):
+        print()
+        Board.print_board()
+        print()
+
 
         print("Options:")
         print("1. Place coin")
         print("2. Pop coin")
         print("3. Quit")
-        userinput = input("Player " + str((turn % 2) + 1) + ": What do you want to do with your coin?\n")
+        userinput = input("Player " + str((turn % 2) + 1) + ", what do you want to do with your coin? ")
+        print()
 
-        if userinput == "3":
+        if userinput in ["3", "Quit", "quit", "q","Q"]:
             running = False
             break
 
@@ -301,22 +353,37 @@ def PvP(num_rows, num_columns):
             else:
                 Board.remove_Coin(int(col))
     
-        print()
-        Board.print_board()
-        print()
-        #Change 2 and 6 to find wins
+    
         if Board.check_win(player_one_symbol):
+            print()
             print("Player 1 wins!!")
+            print()
+            Board.print_board()
+            print()
+            Board.game_over = True
             running = False
         elif Board.check_win(player_two_symbol):
+            print()
             print("Player 2 wins!!")
+            print()
+            Board.print_board()
+            print()
+            Board.game_over = True
             running = False
         elif Board.check_tie():
-            print("We have a tie")
+            print()
+            print("We have a tie!")
+            print()
+            Board.print_board()
+            print()
+            Board.game_over = True
             running = False
         
 
 def PvEasy(num_rows, num_columns):
+    '''
+    Sets up player vs easy ai game mode
+    '''
     AI_voicelines = load_words()
 
     Board = board(num_rows, num_columns)
@@ -324,16 +391,16 @@ def PvEasy(num_rows, num_columns):
     print()
 
     print("Player will enter a character symbol for their coin\n")
-    player_symbol = input("Player: Please enter your symbol\n")
+    player_symbol = input("Player, please enter your symbol: ")
     AI_symbol = "A"
     
     while player_symbol == AI_symbol or player_symbol == "0":
-        print("Same symbols or player's symbol is 0\n")
-        player_symbol = input("Player: Please enter your symbol\n")
+        print("This symbol is not valid. Please enter another symbol. ")
+        player_symbol = input("Player, please enter your symbol: ")
 
     running = True
     while (running):
-
+        print()
         Board.print_board()
         print()
 
@@ -341,9 +408,10 @@ def PvEasy(num_rows, num_columns):
         print("1. Place coin")
         print("2. Pop coin")
         print("3. Quit")
-        userinput = input("Player: What do you want to do with your coin?\n")
+        userinput = input("Player: What do you want to do with your coin? ")
+        print()
 
-        if userinput == "3":
+        if userinput in ["3", "Quit", "quit", "q","Q"]:
             running = False
             break
 
@@ -379,31 +447,48 @@ def PvEasy(num_rows, num_columns):
         
         print()
 
-        #Change 2 and 6 to find wins
+                
         if Board.check_win(player_symbol):
-            print("The player wins!!", end=" ")
-            print("Thanks for playing!")
+            print()
+            Board.print_board()
+            print()
+            print("The player wins!!")
+            Board.game_over = True
             running = False
         elif Board.check_win(AI_symbol):
-            print("The computer wins!!", end=" ")
-            print("Thanks for playing!")
+            print()
+            Board.print_board()
+            print()
+            print("The computer wins!!")
+            Board.game_over = True
             running = False
         elif Board.check_tie():
-            print("We have a tie", end=" ")
-            print("Thanks for playing!")
+            print()
+            Board.print_board()
+            print()
+            print("We have a tie")
+            Board.game_over = True
             running = False
+            print()
+
 
 def PvMod(num_rows, num_columns):
-    Board = board(num_columns, num_rows)
+    '''
+    Sets up player vs moderate ai gamemode
+    '''
+    AI_voicelines = load_words()
     
-
+    Board = board(num_rows, num_columns)
+    
+    print("Your Opponent: Mediocre Matthew!!")
+    print()
     print("Player will enter a character symbol for their coin\n")
-    player_symbol = input("Player: Please enter your symbol\n")
+    player_symbol = input("Player, please enter your symbol: ")
     AI_symbol = "A"
     
     while player_symbol == AI_symbol or player_symbol == "0":
-        print("Same symbols or player's symbol is 0\n")
-        player_symbol = input("Player: Please enter your symbol\n")
+        print("This symbol is not valid. Please enter another symbol. ")
+        player_symbol = input("Player, please enter your symbol: ")
 
     running = True
     while (running):
@@ -416,10 +501,11 @@ def PvMod(num_rows, num_columns):
         print("1. Place coin")
         print("2. Pop coin")
         print("3. Quit")
-        userinput = input("Player: What do you want to do with your coin?\n")
+        userinput = input("Player, what do you want to do with your coin? ")
+        print()
         col = ""
 
-        if userinput == "3":
+        if userinput in ["3", "Quit", "quit", "q","Q"]:
             running = False
             break
 
@@ -444,35 +530,62 @@ def PvMod(num_rows, num_columns):
 
         if (random.randint(1, 100) == 50):
             Board.remove_Coin(int(col))
-            print("AI removed Coin in Column: " + str(AI_column) + ".")
+            print("Matthew removed Coin in Column: " + str(AI_column) + ".")
         else:
             Board.place_coin(AI_column, AI_symbol)
-            print("AI Placed Coin in Column: " + str(AI_column) + ".")
+            print("Matthew placed Coin in Column: " + str(AI_column) + ".")
+
+        randomLine = random.randint(0, len(AI_voicelines) - 1)
+        print("Matthew: \"" + str(AI_voicelines[randomLine]) + "\"")
+        
         
         #Change 2 and 6 to find wins
         if Board.check_win(player_symbol):
+            print()
+            Board.print_board()
+            print()
             print("The player wins!!")
+            Board.game_over = True
             running = False
         elif Board.check_win(AI_symbol):
+            print()
+            Board.print_board()
+            print()
             print("The computer wins!!")
+            Board.game_over = True
             running = False
         elif Board.check_tie():
+            print()
+            Board.print_board()
+            print()
             print("We have a tie")
+            Board.game_over = True
             running = False
+            print()
+        
 
 def main():
-    num_columns = 6
-    num_rows = 7
-
+    '''
+    main function implementation
+    '''
+    num_columns = 7
+    num_rows = 6
+    print()
     print("Welcome to Connect 4++!")
+    print("Made by Samyak, Jay, Shrey")
+    print()
     
     running = True
     while running:
-        print("1. Player vs Easy AI")
-        print("2. Player vs Hard AI")
-        print("3. Player vs Player")
-        print("4. Change Board Size")
-        print("5. Quit")
+        print("Settings: ")
+        print("- Board Size: " + str(num_columns) + " x " + str(num_rows))
+        print()
+        print("Menu: ")
+        print(" 1. Player vs Easy AI")
+        print(" 2. Player vs Hard AI")
+        print(" 3. Player vs Player")
+        print(" 4. Change Board Size")
+        print(" 5. Quit")
         userInput = input("Please select an option: ") #whatever user inputs will be stored in userInput
 
         if userInput == "1":
@@ -485,16 +598,30 @@ def main():
             PvP(num_rows, num_columns)
 
         elif userInput == "4":
+            
             new_column_num = input("Enter board width: ")
             num_columns = int(new_column_num)
             new_row_num = input("Enter board height: ")
             num_rows = int(new_row_num)
+            
+            while (num_columns * num_rows > 4000):
+                print("Error. Board Size is too large. Please make the area of the board less than 4000")
+                print()
+                new_column_num = input("Enter board width: ")
+                num_columns = int(new_column_num)
+                new_row_num = input("Enter board height: ")
+                num_rows = int(new_row_num)
+                
+
+            
 
         elif userInput in ["5", "Quit", "quit", "q","Q"]:
-            print("Thanks for playing!")
             break
  
         print()
+    print()
+    print("Thanks for playing Connect 4++!")
+    print()
 
 if __name__ == "__main__":
     main()
